b27dbc16fa941af2b043204c5dc0cf707da18760
ashu11_01 <writetoashutosh@gmail.com>
2024-09-22 16:59:00 +0530

    Added active spring profile for test

--- src/test/java/net/yafw/forum/ForumApplicationTests.java
+++ src/test/java/net/yafw/forum/ForumApplicationTests.java
@@ -2,8 +2,10 @@ package net.yafw.forum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ForumApplicationTests {
 
 	@Test
